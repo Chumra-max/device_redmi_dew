831617e44ced932f8263a6e66219a8d00d54b133
Chumra-max <chumara126@gmail.com>
2026-07-27 20:32:04 +0100

    Rename product from twrp_dew to omni_dew

--- omni_dew.mk
+++ omni_dew.mk
@@ -1,8 +1,7 @@
 $(call inherit-product, device/redmi/dew/device.mk)
 
-PRODUCT_NAME := twrp_dew
+PRODUCT_NAME := omni_dew
 PRODUCT_DEVICE := dew
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := Xiaomi
-# Confirmed via: adb shell getprop ro.product.model
 PRODUCT_MODEL := 25078RA3EA
