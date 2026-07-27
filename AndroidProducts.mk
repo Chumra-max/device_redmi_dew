3e475bafd874266fa7aa429904c9eb30cb1abb6d
Chumra-max <chumara126@gmail.com>
2026-07-27 20:32:42 +0100

    Replace TWRP with Omni in AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_dew.mk
+    $(LOCAL_DIR)/omni_dew.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_dew-user \
-    twrp_dew-userdebug \
-    twrp_dew-eng
+    omni_dew-user \
+    omni_dew-userdebug \
+    omni_dew-eng
