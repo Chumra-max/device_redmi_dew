064e935ae84faa60120643aac7f3bc8e333067cd
Chumra-max <chumara126@gmail.com>
2026-07-27 22:19:32 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_dew.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_dew-user \
-    omni_dew-userdebug \
-    omni_dew-eng
+    twrp_dew-user \
+    twrp_dew-userdebug \
+    twrp_dew-eng
