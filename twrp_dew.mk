a0f07cd1786aaf5d6f6b5a39131a328dd21de854
Chumra-max <chumara126@gmail.com>
2026-07-27 21:44:54 +0100

    Update twrp_dew.mk

--- twrp_dew.mk
+++ twrp_dew.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/redmi/dew/device.mk)
 
-PRODUCT_NAME := twrp_dew
+PRODUCT_NAME := omni_dew
 PRODUCT_DEVICE := dew
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := Xiaomi
