ce5568728c98d14fd73ce6986357adf11aacd916
Chumra-max <chumara126@gmail.com>
2026-07-30 13:00:18 +0100

    Delete device_redmi_dew-main directory

--- device_redmi_dew-main/AndroidProducts.mk
+++
@@ -1,7 +0,0 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_dew.mk
-
-COMMON_LUNCH_CHOICES := \
-    twrp_dew-ap2a-user \
-    twrp_dew-ap2a-userdebug \
-    twrp_dew-ap2a-eng
--- device_redmi_dew-main/twrp_dew.mk
+++
@@ -1,7 +0,0 @@
-$(call inherit-product, device/redmi/dew/device.mk)
-
-PRODUCT_NAME := twrp_dew
-PRODUCT_DEVICE := dew
-PRODUCT_BRAND := Redmi
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := 25078RA3EA
